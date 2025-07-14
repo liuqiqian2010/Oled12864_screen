d6574d4767036bd6086afb4fb125314c6756ceb7
liuqiqian2010 <liuqiqian@foxmail.com>
2025-07-14 20:17:56 +0800

    first commit

---
+++ MDK-ARM/RTE/_oled12864_screen/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'oled12864_screen' 
+ * Target:  'oled12864_screen' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/oled12864_screen/startup_stm32f10x_md.d
@@ -0,0 +1,1 @@
+oled12864_screen\startup_stm32f10x_md.o: ..\Core\Src\startup_stm32f10x_md.s
